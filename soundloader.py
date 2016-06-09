1f6752974184121037d20b991d28fdcf119cd724
samuelveillette <veillette.samuel@hotmail.com>
2016-06-08 21:39:29 -0400

    pep8 stuff

--- soundloader.py
+++ soundloader.py
@@ -1,6 +1,16 @@
+"""
+soundloader is a simple program to download songs from soundcloud.
+
+soundloader require a download directory, a soundcloud client id
+and a url representing a single track to work. It does not handle
+any error.
+"""
+
 import os
+
 import requests
 import soundcloud
+
 
 DOWNLOAD_DIR = ""
 CLIENT_ID = ""
@@ -12,17 +22,12 @@ class Soundloader:
         self.client = soundcloud.Client(client_id=client_id)
 
     def download_track(self, track_url):
+        """ Download a soundcloud track given a URL """
         track = self.client.get('/resolve', url=track_url)
         stream_url = self.client.get(track.stream_url, allow_redirects=False)
-        audiofile = DOWNLOAD_DIR + track.title +'.mp3'
-
+        audiofile = DOWNLOAD_DIR + track.title + '.mp3'
         with open(audiofile, 'wb') as handle:
             resp = requests.get(stream_url.location, stream=True)
-
-            if not resp.ok:
-                # Something went wrong
-                pass
-
             for block in resp.iter_content(1024):
                 handle.write(block)
 
